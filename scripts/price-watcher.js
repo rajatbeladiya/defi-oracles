5585f9bc6809d60db2e22aabe9c0388a57f8ec59
Rajat Beladiya <rajatbeladiya7@gmail.com>
2021-02-05 17:07:23 +0530

    contracts and scripts added

---
+++ scripts/price-watcher.js
@@ -0,0 +1,24 @@
+const CoinGecko = require('coingecko-api');
+const Oracle = artifacts.require('Oracle.sol');
+
+const POLL_INTERVAL = 5000;
+const CoinGeckoClient = new CoinGecko();
+
+module.exports = async done => {
+  const [_, reporter] = await web3.eth.getAccounts();
+  const oracle = await Oracle.deployed();
+
+  while(true) {
+    const response = await CoinGeckoClient.coins.fetch('bitcoin', {});
+    let currentPrice = parseFloat(response.data.market_data.current_price.usd);
+    currentPrice = parseInt(currentPrice * 100);
+    await oracle.updateData(
+      web3.utils.soliditySha3('BTC/USD'),
+      currentPrice,
+      { from: reporter }
+    );
+    console.log(`new price for BTC/USD ${currentPrice} update on-chain`);
+    await new Promise(resolve => setTimeout(resolve, POLL_INTERVAL));
+  }
+  // done();
+}
